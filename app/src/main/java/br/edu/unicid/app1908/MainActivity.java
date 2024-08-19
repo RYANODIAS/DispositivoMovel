9fb3e9e9b363fb8f02380916440e0ee5668d85f7
Auto Logon <autologon@CSED.LAB>
2024-08-19 09:56:20 -0300

    Conteudo da aula de programacao de dispositivos móveis

---
+++ app/src/main/java/br/edu/unicid/app1908/MainActivity.java
@@ -0,0 +1,15 @@
+package br.edu.unicid.app1908;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
